df21c3f361f3d946d2e03dbb11ce25e456bff2f8
chlee1001 <chlee1001@naver.com>
2023-04-18 15:58:29 +0900

    유지할 압축파일개수를 첫번째 파라미터로 전달받을 수 있도록 수정, 변수명 수정, 압축성공 시 파일사이즈를 로그에 남기도록 추가

--- backup/backup_dir.sh
+++ backup/backup_dir.sh
@@ -1,40 +1,50 @@
 #!/bin/bash
 
 # Set the path to the folder you want to backup
-# BACKUP_PATH="/var/lib/"
-# BACKUP_FOLDER_NAME="jenkins"
-BACKUP_PATH="/home/ubuntu/rudy"
-BACKUP_FOLDER_NAME="test_folder"
+# BACKUP_PARENT_DIR="/var/lib/"
+# BACKUP_DIR="jenkins"
+BACKUP_PARENT_DIR="/home/ubuntu/rudy"
+BACKUP_DIR="test_folder"
 
 # Set the path to where you want to save the backups
-BACKUP_DIR="/home/ubuntu/rudy_backup_test"
+DESTINATION_DIR="/home/ubuntu/rudy_backup_test"
 
 # Set the path to where you want to save the log file
 LOG_FILE="/home/ubuntu/rudy_backup_test/logfile.txt"
 
-# Create the backup directory if it doesn't exist
-if [ ! -d "$BACKUP_DIR" ]; then
-  echo "Creating backup jenkins directory: $BACKUP_DIR" >> "$LOG_FILE"
-  mkdir -p "$BACKUP_DIR"
-fi
-
 # Get the current date in the format we want for the backup filename
-BACKUP_DATE=$(date +"%Y-%m-%d_%H-%M-%S")
+BACKUP_DATE=$(date +"%Y%m%d-%H%M%S")
 
 # Create the backup filename
-BACKUP_FILENAME="${BACKUP_FOLDER_NAME}_${BACKUP_DATE}.tar.gz"
+BACKUP_FILENAME="${BACKUP_DIR}_${BACKUP_DATE}.tar.gz"
+
+# Get the MAX_KEEP_BACKUPS parameter or set the default value
+MAX_KEEP_BACKUPS=${1:-10}
+
+# Create the backup directory if it doesn't exist
+if [ ! -d "${DESTINATION_DIR}" ]; then
+  echo "Creating backup ${BACKUP_DIR} directory: ${DESTINATION_DIR}" >> "${LOG_FILE}"
+  mkdir -p "${DESTINATION_DIR}"
+fi
 
 # Create the backup
 echo "Creating backup: $BACKUP_FILENAME" >> "$LOG_FILE"
-cd "${BACKUP_PATH}"
+cd "${BACKUP_PARENT_DIR}"
 pwd
-tar cfz "${BACKUP_DIR}/${BACKUP_FILENAME}" "./$BACKUP_FOLDER_NAME"
+tar cfz "${DESTINATION_DIR}/${BACKUP_FILENAME}" "./${BACKUP_DIR}"
+backup_size=$(du -h "${DESTINATION_DIR}/${BACKUP_FILENAME}" | cut -f1)
 
+if [ "$?" -eq 0 ]; then
+  echo "Backup created successfully. File size: $backup_size" >> "$LOG_FILE"
+else
+  echo "Backup creation failed" >> "$LOG_FILE"
+fi
 
-# Delete backups if there are more than 5 files in the backup directory
-NUM_BACKUPS=$(ls -1 "$BACKUP_DIR"/*.tar.gz | wc -l)
-if [ $NUM_BACKUPS -gt 10 ]; then
-  echo "Deleting old backups" >> "$LOG_FILE"
-  DELETE_COUNT=$((NUM_BACKUPS - 5))
-  ls -1t "$BACKUP_DIR"/*.tar.gz | tail -$DELETE_COUNT | xargs rm --
+# Delete backups if there are more than ${MAX_KEEP_BACKUPS} files in the backup directory
+NUM_BACKUPS=$(ls -1 "${DESTINATION_DIR}"/*.tar.gz | wc -l)
+echo ${NUM_BACKUPS}
+if [ $NUM_BACKUPS -gt ${MAX_KEEP_BACKUPS} ]; then
+  DELETE_COUNT=$((NUM_BACKUPS - ${MAX_KEEP_BACKUPS}))
+  echo "Deleting old backups: ${DELETE_COUNT}" >> "$LOG_FILE"
+  ls -1t "${DESTINATION_DIR}"/*.tar.gz | tail -$DELETE_COUNT | xargs rm --
 fi
